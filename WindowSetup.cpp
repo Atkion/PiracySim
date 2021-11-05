d2d4cbaf2917bc64c0c1bf66672adb0a35f58d87
atkion <giddavidr@gmail.com>
2021-11-05 15:06:58 -0600

    Adding WIndowSetup for env consistency

---
+++ WindowSetup.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+//the following line is necessary for the GetConsoleWindow() function to work!
+//it basically says that you are running this program on Windows 2000 or higher
+#define _WIN32_WINNT 0x0500
+//it is important that the above line be typed BEFORE <windows.h> is included
+#include <windows.h>
+#include "WindowSetup.h"
+using namespace std;
+
+void setupGameWindow() {
+  int width = 1200, height = 800;
+  HWND console = GetConsoleWindow();
+  RECT r, d;
+  GetWindowRect(console, &r); //stores the console's current dimensions
+  GetWindowRect(GetDesktopWindow(), &d); //stores the desktop dimensions
+  int drawX = d.right - width - (d.right - width)/2, drawY = d.bottom - height - (d.bottom - height)/2; // center window
+  MoveWindow(console, drawX, drawY, width, height, TRUE); //MoveWindow(window_handle, x, y, width, height, redraw_window);
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "WindowSetup.h"
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main (void)
+{
+  setupGameWindow();
+  printf("WeirdChamp");
+  int i; cin >> i; //This is just to keep the console open because nothing else keeps the game running
+}
